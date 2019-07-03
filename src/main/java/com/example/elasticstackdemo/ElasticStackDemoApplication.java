edab5adb8ee8d67bfa03ded132792201f7e48012
Natthaphon Phanit <natthaphon.pha@ascendcorp.com>
2019-07-03 11:06:29 +0700

    first commit

---
+++ src/main/java/com/example/elasticstackdemo/ElasticStackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.elasticstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElasticStackDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElasticStackDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/elasticstackdemo/ElasticStackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.elasticstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElasticStackDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
