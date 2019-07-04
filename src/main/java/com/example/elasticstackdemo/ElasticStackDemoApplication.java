ef751a554094f3136cee453e241ccf0d09434733
Natthaphon Phanit <natthaphon.pha@ascendcorp.com>
2019-07-04 19:13:10 +0700

    add docker compose

--- src/main/java/com/example/elasticstackdemo/ElasticStackDemoApplication.java
+++ src/main/java/com/example/elasticstackdemo/ElasticStackDemoApplication.java
@@ -1,13 +1,24 @@
 package com.example.elasticstackdemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ElasticStackDemoApplication {
+public class ElasticStackDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ElasticStackDemoApplication.class, args);
 	}
+
+  @Override
+  public void run(String... args) throws Exception {
+    Logger logger = LoggerFactory.getLogger(getClass());
+    for (int i = 0; i < 10; ++i) {
+      logger.error("number: {}", i);
+    }
+  }
 
 }
