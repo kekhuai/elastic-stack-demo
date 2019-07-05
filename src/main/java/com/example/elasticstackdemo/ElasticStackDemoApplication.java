ebec8a65ad2f96e9b238531dc60166189c1726f7
Natthaphon Phanit <natthaphon.pha@ascendcorp.com>
2019-07-05 10:32:56 +0700

    add multiline pattern to filebeat

--- src/main/java/com/example/elasticstackdemo/ElasticStackDemoApplication.java
+++ src/main/java/com/example/elasticstackdemo/ElasticStackDemoApplication.java
@@ -1,12 +1,18 @@
 package com.example.elasticstackdemo;
 
+import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(path = "/api/v1/elk")
 public class ElasticStackDemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
@@ -16,9 +22,17 @@ public class ElasticStackDemoApplication implements CommandLineRunner {
   @Override
   public void run(String... args) throws Exception {
     Logger logger = LoggerFactory.getLogger(getClass());
-    for (int i = 0; i < 10; ++i) {
+    int i = 0;
+    while (true) {
+      TimeUnit.SECONDS.sleep(1);
       logger.error("number: {}", i);
+      ++i;
     }
+  }
+
+  @GetMapping(path = "/ex")
+  public void exception() {
+    throw new RuntimeException("example stack trace");
   }
 
 }
